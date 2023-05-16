9fbf0a84837272033bbeb5940a4f36ffbbda6133
jinwoo park <jwpark@watechnology.co.kr>
2023-05-16 09:35:33 +0900

    update gitignore

---
+++ makeExeFile.bat
@@ -0,0 +1,3 @@
+D:
+cd D:\9999.TEST\Python-OPC-sample
+pyinstaller -w -F --i==icon.ico Python_OPC_sample.py
--- opchelper.py
+++ opchelper.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 import datetime
 
-# 단일 노드의 현재 값 출력 함수 정의
 def get_current_node_value(client, nodeid):
-    # 가져올 노드의 ID를 문자열로 지정
     node = client.get_node(nodeid)    
     #print(f"node: {node}, Value: {node.get_value()}, Timestamp : {datetime.datetime.now()}")
     rtnVal = f"node: {node}, Value: {node.get_value()}, Timestamp : {datetime.datetime.now()}"
     return rtnVal
-    
 
-# 다수 노드의 현재 값 출력 함수 정의
 def get_current_nodes_values(client, node_list):
     rtnVal = ""
     #node_list = [
@@ -25,35 +21,31 @@ def get_current_nodes_values(client, node_list):
         rtnVal += f"node: {node_list[i]}, Value: {value}, Timestamp : {datetime.datetime.now()} \n"
     return rtnVal
 
-# 단일 노드의 과거 값(History) 출력 함수 정의
 def get_histroy_node_value(client, nodeid, stdt, enddt):
     rtnVal = ""
     #end_time = datetime.datetime.now()
     #start_time = end_time - datetime.timedelta(hours=1)
-    end_time = enddt
-    start_time = stdt
+    end_time = enddt - datetime.timedelta(hours=9) # -9hour
+    start_time = stdt - datetime.timedelta(hours=9) # -9hour
     #nodeid="ns=12;i=29576"
-    # 선택한 시간 범위에서 노드의 과거 데이터를 가져온다
-    # 주어진 시간에 값이 없으면, 마지막으로 기록된 값을 가져온다
     node = client.get_node(nodeid)    
     response = node.read_raw_history(start_time, end_time, 3600) 
     for result in response:
         #print(f"node: {node}, Value: {result.Value.Value} Timestamp: {result.SourceTimestamp}")
-        rtnVal += f"node: {node}, Value: {result.Value.Value} Timestamp: {result.SourceTimestamp} \n"
+        rtnVal += f"node: {node}, Value: {result.Value.Value} Timestamp: {result.SourceTimestamp + datetime.timedelta(hours=9)} \n"
     return rtnVal
 
-# 다수 노드의 과거 값(Histroy) 출력
 def get_history_nodes_values(client, node_list, stdt, enddt):
     rtnVal = ""
     #end_time = datetime.datetime.now()
     #start_time = end_time - datetime.timedelta(hours=1)
-    end_time = enddt
-    start_time = stdt
+    end_time = enddt - datetime.timedelta(hours=9)  # -9hour
+    start_time = stdt - datetime.timedelta(hours=9)  # -9hour
     node_list = list(map(client.get_node, node_list))
     for node in node_list:
         response = node.read_raw_history(start_time, end_time, 3600)
         for result in response:
             #print(f"node: {node}, Value: {result.Value.Value} Timestamp: {result.SourceTimestamp}")
-            rtnVal += f"node: {node}, Value: {result.Value.Value} Timestamp: {result.SourceTimestamp} \n"
+            rtnVal += f"node: {node}, Value: {result.Value.Value} Timestamp: {result.SourceTimestamp + datetime.timedelta(hours=9)} \n"
     return rtnVal
 
